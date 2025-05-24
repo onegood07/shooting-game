395f035e6f8308d3270370f37d7e9f6a9ee5db4c
ryanzxc1 <ryan17126@naver.com>
2025-05-24 20:19:54 +0900

    feat: 생성자 함수 구현

--- src/player/player.py
+++ src/player/player.py
@@ -1,1 +1,16 @@
 # player.py
+
+import pygame
+
+class Player:
+    def __init__(self):
+        self.x=100
+        self.y=500
+        self.radius = 25
+        self.color = (50,50,50)
+        self.speed=5
+        self.jump_velocity=0
+        self.is_jumping=False
+        self.bullets=[] 
+
+    
